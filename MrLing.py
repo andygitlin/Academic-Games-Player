5b1f460a3be759454a320bb7e4eb6e1e60e728bb
yuxuanc <yuxuanc@umich.edu>
2018-03-15 08:01:27 -0400

    add stuff for heroku deployment

--- MrLing.py
+++ MrLing.py
@@ -945,6 +945,6 @@ def home():
 ################################################## main
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0', port = int(os.getenv('PORT', 5000)))
 
 
